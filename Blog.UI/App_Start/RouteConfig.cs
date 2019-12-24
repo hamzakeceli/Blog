89a3f3c54181adeb11155efb4d777f97657d4750
Hamza KEÇELİ <hamza.keceli@hotmail.com>
2019-12-24 19:22:47 +0300

    admin page start

--- Blog.UI/App_Start/RouteConfig.cs
+++ Blog.UI/App_Start/RouteConfig.cs
@@ -16,8 +16,9 @@ namespace Blog.UI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "Blog.UI.Controllers" }
+                );
         }
     }
 }
---
+++ Blog.UI/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Web.Mvc;
+
+namespace Blog.UI.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Admin/Home
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
